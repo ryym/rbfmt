b2f1c03ec3bbd96c3f1649e471ea136cc510f491
ryym <ryym.64@gmail.com>
2024-02-10 12:17:54 +0900

    refactor(fmt/case): Extract Case formatting responsibility

--- src/fmt/node/case.rs
+++ src/fmt/node/case.rs
@@ -1,4 +1,9 @@
-use crate::fmt::{shape::Shape, LeadingTrivia, TrailingTrivia};
+use crate::fmt::{
+    output::{FormatContext, Output},
+    shape::Shape,
+    trivia::EmptyLineHandling,
+    LeadingTrivia, TrailingTrivia,
+};
 
 use super::{Else, Node, Statements};
 
@@ -14,6 +19,70 @@ pub(crate) struct Case {
 impl Case {
     pub(crate) fn shape() -> Shape {
         Shape::Multilines
+    }
+
+    pub(crate) fn format(&self, o: &mut Output, ctx: &FormatContext) {
+        o.push_str("case");
+        match &self.predicate {
+            Some(pred) => {
+                if pred.shape.fits_in_one_line(o.remaining_width) || pred.is_diagonal() {
+                    o.push(' ');
+                    o.format(pred, ctx);
+                    o.write_trailing_comment(&pred.trailing_trivia);
+                } else {
+                    o.indent();
+                    o.break_line(ctx);
+                    o.write_leading_trivia(
+                        &pred.leading_trivia,
+                        ctx,
+                        EmptyLineHandling::Trim {
+                            start: true,
+                            end: true,
+                        },
+                    );
+                    o.format(pred, ctx);
+                    o.write_trailing_comment(&pred.trailing_trivia);
+                    o.dedent();
+                }
+            }
+            None => {
+                o.write_trailing_comment(&self.case_trailing);
+            }
+        }
+        if self.first_branch_leading.is_empty() {
+            o.break_line(ctx);
+        } else {
+            o.indent();
+            o.break_line(ctx);
+            o.write_leading_trivia(
+                &self.first_branch_leading,
+                ctx,
+                EmptyLineHandling::Trim {
+                    start: true,
+                    end: true,
+                },
+            );
+            o.dedent();
+        }
+        for (i, branch) in self.branches.iter().enumerate() {
+            if i > 0 {
+                o.break_line(ctx);
+            }
+            branch.format(o, ctx);
+        }
+        if let Some(otherwise) = &self.otherwise {
+            o.break_line(ctx);
+            o.push_str("else");
+            o.write_trailing_comment(&otherwise.keyword_trailing);
+            if !otherwise.body.shape.is_empty() {
+                o.indent();
+                o.break_line(ctx);
+                otherwise.body.format(o, ctx, true);
+                o.dedent();
+            }
+        }
+        o.break_line(ctx);
+        o.push_str("end");
     }
 }
 
@@ -49,5 +118,84 @@ impl CaseWhen {
     pub(crate) fn set_body(&mut self, body: Statements) {
         self.shape.append(&body.shape);
         self.body = body;
+    }
+
+    fn format(&self, o: &mut Output, ctx: &FormatContext) {
+        o.push_str("when");
+        if self.shape.fits_in_one_line(o.remaining_width) {
+            o.push(' ');
+            for (i, cond) in self.conditions.iter().enumerate() {
+                if i > 0 {
+                    o.push_str(", ");
+                }
+                o.format(cond, ctx);
+                o.write_trailing_comment(&cond.trailing_trivia);
+            }
+            if !self.body.shape.is_empty() {
+                o.push_str(" then ");
+                self.body.format(o, ctx, false);
+            }
+        } else {
+            if self.conditions_shape.fits_in_one_line(o.remaining_width) {
+                for (i, cond) in self.conditions.iter().enumerate() {
+                    if i == 0 {
+                        o.push(' ');
+                    } else {
+                        o.push_str(", ");
+                    }
+                    o.format(cond, ctx);
+                    o.write_trailing_comment(&cond.trailing_trivia);
+                }
+            } else {
+                if self.conditions[0].is_diagonal() {
+                    o.push(' ');
+                    o.format(&self.conditions[0], ctx);
+                } else {
+                    o.indent();
+                    o.break_line(ctx);
+                    o.write_leading_trivia(
+                        &self.conditions[0].leading_trivia,
+                        ctx,
+                        EmptyLineHandling::Trim {
+                            start: true,
+                            end: true,
+                        },
+                    );
+                    o.format(&self.conditions[0], ctx);
+                    o.dedent();
+                }
+                if self.conditions.len() > 1 {
+                    o.push(',');
+                }
+                o.write_trailing_comment(&self.conditions[0].trailing_trivia);
+                if self.conditions.len() > 1 {
+                    o.indent();
+                    let last_idx = self.conditions.len() - 1;
+                    for (i, cond) in self.conditions.iter().enumerate().skip(1) {
+                        o.break_line(ctx);
+                        o.write_leading_trivia(
+                            &cond.leading_trivia,
+                            ctx,
+                            EmptyLineHandling::Trim {
+                                start: false,
+                                end: false,
+                            },
+                        );
+                        o.format(cond, ctx);
+                        if i < last_idx {
+                            o.push(',');
+                        }
+                        o.write_trailing_comment(&cond.trailing_trivia);
+                    }
+                    o.dedent();
+                }
+            }
+            if !self.body.shape.is_empty() {
+                o.indent();
+                o.break_line(ctx);
+                self.body.format(o, ctx, true);
+                o.dedent();
+            }
+        }
     }
 }
