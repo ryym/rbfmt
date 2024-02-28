2fa33a68fc65e77f9d173e3698f5609596dc8037
ryym <ryym.64@gmail.com>
2024-02-28 23:08:49 +0900

    refactor(parse): Move undef/defined/preexec/postexec/alias parsing to file

--- src/parse.rs
+++ src/parse.rs
@@ -8,6 +8,7 @@ mod hashes;
 mod ifs;
 mod loops;
 mod methods;
+mod miscs;
 mod pattern_matches;
 mod postmodifiers;
 mod ranges;
@@ -518,13 +519,11 @@ impl Parser<'_> {
 
             prism::Node::UndefNode { .. } => {
                 let node = node.as_undef_node().unwrap();
-                let call_like = self.visit_undef(node);
-                fmt::Node::new(fmt::Kind::CallLike(call_like))
+                self.parse_undef(node)
             }
             prism::Node::DefinedNode { .. } => {
                 let node = node.as_defined_node().unwrap();
-                let call_like = self.visit_defined(node);
-                fmt::Node::new(fmt::Kind::CallLike(call_like))
+                self.parse_defined(node)
             }
 
             prism::Node::BeginNode { .. } => {
@@ -625,35 +624,30 @@ impl Parser<'_> {
 
             prism::Node::PreExecutionNode { .. } => {
                 let node = node.as_pre_execution_node().unwrap();
-                let exec = self.visit_pre_post_exec(
+                self.parse_pre_post_exec(
                     node.keyword_loc(),
                     node.opening_loc(),
                     node.statements(),
                     node.closing_loc(),
-                );
-                fmt::Node::new(fmt::Kind::PrePostExec(exec))
+                )
             }
             prism::Node::PostExecutionNode { .. } => {
                 let node = node.as_post_execution_node().unwrap();
-                let exec = self.visit_pre_post_exec(
+                self.parse_pre_post_exec(
                     node.keyword_loc(),
                     node.opening_loc(),
                     node.statements(),
                     node.closing_loc(),
-                );
-                fmt::Node::new(fmt::Kind::PrePostExec(exec))
+                )
             }
 
             prism::Node::AliasMethodNode { .. } => {
                 let node = node.as_alias_method_node().unwrap();
-                let (leading, alias) = self.visit_alias(node.new_name(), node.old_name());
-                println!("AA {:?}", leading);
-                fmt::Node::with_leading_trivia(leading, fmt::Kind::Alias(alias))
+                self.parse_alias(node.new_name(), node.old_name())
             }
             prism::Node::AliasGlobalVariableNode { .. } => {
                 let node = node.as_alias_global_variable_node().unwrap();
-                let (leading, alias) = self.visit_alias(node.new_name(), node.old_name());
-                fmt::Node::with_leading_trivia(leading, fmt::Kind::Alias(alias))
+                self.parse_alias(node.new_name(), node.old_name())
             }
 
             _ => todo!("parse {:?}", node),
@@ -888,40 +882,6 @@ impl Parser<'_> {
         block_params
     }
 
-    fn visit_undef(&mut self, undef: prism::UndefNode) -> fmt::CallLike {
-        let mut args = fmt::Arguments::new(None, None);
-        Self::each_node_with_trailing_end(undef.names().iter(), None, |node, trailing_end| {
-            let node = self.visit(node, trailing_end);
-            args.append_node(node);
-        });
-        let mut call_like = fmt::CallLike::new("undef".to_string());
-        call_like.set_arguments(args);
-        call_like
-    }
-
-    fn visit_defined(&mut self, defined: prism::DefinedNode) -> fmt::CallLike {
-        let lparen_loc = defined.lparen_loc();
-        let rparen_loc = defined.rparen_loc();
-
-        let value = defined.value();
-        let value_next = rparen_loc.as_ref().map(|l| l.start_offset());
-        let value = self.visit(value, value_next);
-
-        let lparen = lparen_loc.as_ref().map(Self::source_lossy_at);
-        let rparen = rparen_loc.as_ref().map(Self::source_lossy_at);
-        let mut args = fmt::Arguments::new(lparen, rparen);
-        args.last_comma_allowed = false;
-        args.append_node(value);
-
-        let rparen_start = rparen_loc.as_ref().map(|l| l.start_offset());
-        let virutla_end = self.take_end_trivia_as_virtual_end(rparen_start);
-        args.set_virtual_end(virutla_end);
-
-        let mut call_like = fmt::CallLike::new("defined?".to_string());
-        call_like.set_arguments(args);
-        call_like
-    }
-
     fn parse_block_body(
         &mut self,
         body: Option<prism::Node>,
@@ -1094,33 +1054,6 @@ impl Parser<'_> {
             let fmt_node = self.visit(node, trailing_end);
             f(fmt_node);
         });
-    }
-
-    fn visit_pre_post_exec(
-        &mut self,
-        keyword_loc: prism::Location,
-        opening_loc: prism::Location,
-        statements: Option<prism::StatementsNode>,
-        closing_loc: prism::Location,
-    ) -> fmt::PrePostExec {
-        let keyword = Self::source_lossy_at(&keyword_loc);
-        let closing_start = closing_loc.start_offset();
-        let was_flat = !self.does_line_break_exist_in(opening_loc.end_offset(), closing_start);
-        let statements = self.visit_statements(statements, Some(closing_start));
-        fmt::PrePostExec::new(keyword, statements, was_flat)
-    }
-
-    fn visit_alias(
-        &mut self,
-        new_name: prism::Node,
-        old_name: prism::Node,
-    ) -> (fmt::LeadingTrivia, fmt::Alias) {
-        let additional_leading = self.take_leading_trivia(new_name.location().start_offset());
-        let old_loc = old_name.location();
-        let new_name = self.visit(new_name, Some(old_loc.start_offset()));
-        let old_name = self.visit(old_name, None);
-        let alias = fmt::Alias::new(new_name, old_name);
-        (additional_leading, alias)
     }
 
     fn wrap_as_statements(&mut self, node: Option<fmt::Node>, end: usize) -> fmt::Statements {
