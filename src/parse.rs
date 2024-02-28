effa4632dd8af2f00cb2a9ebd022065a2ea8f207
ryym <ryym.64@gmail.com>
2024-02-28 22:57:23 +0900

    refactor(parse): Move class parsing to file

--- src/parse.rs
+++ src/parse.rs
@@ -3,6 +3,7 @@ mod assigns;
 mod atoms;
 mod calls;
 mod cases;
+mod classes;
 mod hashes;
 mod ifs;
 mod loops;
@@ -545,45 +546,27 @@ impl Parser<'_> {
 
             prism::Node::ClassNode { .. } => {
                 let node = node.as_class_node().unwrap();
-                let (leading, class) = self.visit_class_like(
+                self.parse_class_like(
                     "class",
                     node.constant_path().location(),
                     node.superclass(),
                     node.body(),
                     node.end_keyword_loc(),
-                );
-                fmt::Node::with_leading_trivia(leading, fmt::Kind::ClassLike(class))
+                )
             }
             prism::Node::ModuleNode { .. } => {
                 let node = node.as_module_node().unwrap();
-                let (leading, module) = self.visit_class_like(
+                self.parse_class_like(
                     "module",
                     node.constant_path().location(),
                     None,
                     node.body(),
                     node.end_keyword_loc(),
-                );
-                fmt::Node::with_leading_trivia(leading, fmt::Kind::ClassLike(module))
+                )
             }
             prism::Node::SingletonClassNode { .. } => {
                 let node = node.as_singleton_class_node().unwrap();
-                let leading = self.take_leading_trivia(node.operator_loc().start_offset());
-                let body = node.body();
-                let end_loc = node.end_keyword_loc();
-                let body_start = body.as_ref().and_then(|b| match b {
-                    prism::Node::BeginNode { .. } => {
-                        Self::start_of_begin_block_content(b.as_begin_node().unwrap())
-                    }
-                    _ => Some(b.location().start_offset()),
-                });
-                let expr_next = body_start.unwrap_or(end_loc.start_offset());
-                let expr = self.visit(node.expression(), Some(expr_next));
-                let body = self.parse_block_body(body, end_loc.start_offset());
-                let class = fmt::SingletonClass {
-                    expression: Box::new(expr),
-                    body,
-                };
-                fmt::Node::with_leading_trivia(leading, fmt::Kind::SingletonClass(class))
+                self.parse_singleton_class(node)
             }
 
             prism::Node::RangeNode { .. } => {
@@ -1215,43 +1198,6 @@ impl Parser<'_> {
             let fmt_node = self.visit(node, trailing_end);
             f(fmt_node);
         });
-    }
-
-    fn visit_class_like(
-        &mut self,
-        keyword: &str,
-        name_loc: prism::Location,
-        superclass: Option<prism::Node>,
-        body: Option<prism::Node>,
-        end_loc: prism::Location,
-    ) -> (fmt::LeadingTrivia, fmt::ClassLike) {
-        let leading = self.take_leading_trivia(name_loc.start_offset());
-        let name = Self::source_lossy_at(&name_loc);
-
-        let body_start = body.as_ref().and_then(|b| match b {
-            prism::Node::BeginNode { .. } => {
-                Self::start_of_begin_block_content(b.as_begin_node().unwrap())
-            }
-            _ => Some(b.location().start_offset()),
-        });
-        let head_next = body_start.unwrap_or(end_loc.start_offset());
-        let (superclass, head_trailing) = if let Some(superclass) = superclass {
-            let fmt_node = self.visit(superclass, Some(head_next));
-            (Some(fmt_node), fmt::TrailingTrivia::none())
-        } else {
-            let head_trailing = self.take_trailing_comment(head_next);
-            (None, head_trailing)
-        };
-
-        let body = self.parse_block_body(body, end_loc.start_offset());
-        let class = fmt::ClassLike {
-            keyword: keyword.to_string(),
-            name,
-            superclass: superclass.map(Box::new),
-            head_trailing,
-            body,
-        };
-        (leading, class)
     }
 
     fn visit_array_pattern(&mut self, node: prism::ArrayPatternNode) -> fmt::ArrayPattern {
