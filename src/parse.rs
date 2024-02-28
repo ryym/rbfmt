e7ea64002aa41f08d8b6e4756b884c1dcd96108b
ryym <ryym.64@gmail.com>
2024-02-28 23:51:51 +0900

    refactor(parse): Move trivia parsing to file

--- src/parse.rs
+++ src/parse.rs
@@ -21,9 +21,10 @@ mod src;
 mod statements;
 mod strings;
 mod symbols;
+mod trivia;
 
 use crate::fmt;
-use std::{collections::HashMap, iter::Peekable, ops::Range};
+use std::{collections::HashMap, iter::Peekable};
 
 pub(crate) fn parse_into_fmt_node(source: Vec<u8>) -> Option<ParserResult> {
     let result = prism::parse(&source);
@@ -651,107 +652,6 @@ impl Parser<'_> {
                 f(element);
             },
         );
-    }
-
-    fn take_end_trivia_as_virtual_end(&mut self, end: Option<usize>) -> Option<fmt::VirtualEnd> {
-        if let Some(end) = end {
-            let trivia = self.take_leading_trivia(end);
-            if !trivia.is_empty() {
-                return Some(fmt::VirtualEnd::new(trivia));
-            }
-        }
-        None
-    }
-
-    fn take_leading_trivia(&mut self, loc_start: usize) -> fmt::LeadingTrivia {
-        let mut trivia = fmt::LeadingTrivia::new();
-
-        while let Some(comment) = self.comments.peek() {
-            let loc = comment.location();
-            if !(self.last_loc_end..=loc_start).contains(&loc.start_offset()) {
-                break;
-            };
-            let mut value = Self::source_lossy_at(&loc);
-            if value.starts_with("=begin") {
-                value = value.trim_end().to_string();
-            }
-            let fmt_comment = fmt::Comment { value };
-            self.take_empty_lines_until(loc.start_offset(), &mut trivia);
-            trivia.append_line(fmt::LineTrivia::Comment(fmt_comment));
-            self.last_loc_end = loc.end_offset() - 1;
-            self.comments.next();
-        }
-
-        self.take_empty_lines_until(loc_start, &mut trivia);
-        trivia
-    }
-
-    fn take_empty_lines_until(&mut self, end: usize, trivia: &mut fmt::LeadingTrivia) {
-        let range = self.last_empty_line_range_within(self.last_loc_end, end);
-        if let Some(range) = range {
-            trivia.append_line(fmt::LineTrivia::EmptyLine);
-            self.last_loc_end = range.end;
-        }
-    }
-
-    fn take_trailing_comment(&mut self, end: usize) -> fmt::TrailingTrivia {
-        if let Some(comment) = self.comments.peek() {
-            let loc = comment.location();
-            if (self.last_loc_end..=end).contains(&loc.start_offset())
-                && !self.is_at_line_start(loc.start_offset())
-            {
-                self.last_loc_end = loc.end_offset() - 1;
-                self.comments.next();
-                let value = Self::source_lossy_at(&loc);
-                let comment = Some(fmt::Comment { value });
-                return fmt::TrailingTrivia::new(comment);
-            }
-        };
-        fmt::TrailingTrivia::none()
-    }
-
-    fn last_empty_line_range_within(&self, start: usize, end: usize) -> Option<Range<usize>> {
-        let mut line_start: Option<usize> = None;
-        let mut line_end: Option<usize> = None;
-        for i in (start..end).rev() {
-            let b = self.src[i];
-            if b == b'\n' {
-                if line_end.is_none() {
-                    line_end = Some(i + 1);
-                } else {
-                    line_start = Some(i);
-                    break;
-                }
-            } else if line_end.is_some() && b != b' ' {
-                line_end = None;
-            }
-        }
-        match (line_start, line_end) {
-            (Some(start), Some(end)) => Some(start..end),
-            _ => None,
-        }
-    }
-
-    fn is_at_line_start(&self, start: usize) -> bool {
-        if start == 0 {
-            return true;
-        }
-        let mut idx = start - 1;
-        let mut has_char_between_last_newline = false;
-        while let Some(b) = self.src.get(idx) {
-            match b {
-                b' ' => {
-                    idx -= 1;
-                    continue;
-                }
-                b'\n' => break,
-                _ => {
-                    has_char_between_last_newline = true;
-                    break;
-                }
-            }
-        }
-        !has_char_between_last_newline
     }
 
     fn does_line_break_exist_in(&self, start: usize, end: usize) -> bool {
